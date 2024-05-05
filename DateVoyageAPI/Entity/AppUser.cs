951e2c1e5fe4a8efd309d123b7ad6c1beb9550f7
ghgasimovhikmat <gh.gasimovhikmat@gmail.com>
2024-05-05 15:29:38 +0300

    extending entities

--- DateVoyageAPI/Entity/AppUser.cs
+++ DateVoyageAPI/Entity/AppUser.cs
@@ -1,4 +1,6 @@
-﻿namespace DateVoyage.Entity
+﻿using DateVoyage.Extensions;
+
+namespace DateVoyage.Entity
 {
     public class AppUser
     {
@@ -6,7 +8,10 @@
         public string UserName { get;set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string KnownAs { get; set; }
         public DateOnly DateOfBirth { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
@@ -14,5 +19,10 @@
         public string City { get; set; }
         public string Country { get; set; }
         public List<Photo> Photos { get; set; } = new();
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
--- DateVoyageAPI/Entity/Photo.cs
+++ DateVoyageAPI/Entity/Photo.cs
@@ -6,8 +6,9 @@ namespace DateVoyage.Entity
     public class Photo
     {
         public int Id { get; set; }
-        public string Url { get; set; }
+        public string? Url { get; set; }
         public bool IsMain { get; set; }
+        public string? PublicId { get; set; }
         public int AppUserId { get; set; }
         public AppUser AppUser{ get; set; }
 
